112d0c6be233f4d59b004a04042c1c24a7b06bca
Markus B. Schenkel <github@markus-schenkel.ch>
2020-12-29 22:55:10 +0100

    Improved import * Permissions * Jobs * Job-Extras (car)

--- bioco/management/commands/import_legacy.py
+++ bioco/management/commands/import_legacy.py
@@ -1,16 +1,11 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import User, Group, Permission
 from django.core.management.base import BaseCommand, CommandError
 from juntagrico.entity.depot import Depot
-from juntagrico.entity.member import Member
-from juntagrico.entity.member import SubscriptionMembership
-from juntagrico.entity.subs import Subscription
-from juntagrico.entity.subs import SubscriptionPart
-from juntagrico.entity.subtypes import SubscriptionSize
-from juntagrico.entity.subtypes import SubscriptionType
-from juntagrico.entity.subtypes import SubscriptionProduct
+from juntagrico.entity.member import Member, SubscriptionMembership
+from juntagrico.entity.subs import Subscription, SubscriptionPart
+from juntagrico.entity.subtypes import SubscriptionSize, SubscriptionType, SubscriptionProduct
 from juntagrico.entity.share import Share
-from juntagrico.entity.jobs import ActivityArea
+from juntagrico.entity.jobs import ActivityArea, RecuringJob, JobType, Assignment, JobExtraType, JobExtra
 
 import datetime
 from django.utils.timezone import make_aware
@@ -25,24 +20,38 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         db_file_name = options['db_file_name']
-        print(f"Opening {db_file_name}")
+        print(f"=== Importing from {db_file_name} ===")
         with open(db_file_name, 'r') as infile:
             data = json.load(infile)
 
             self.create_abo_structure()
+            self.create_job_extras()
 
             self.run_import(data, 'auth.group',                 'Group',        self.import_group)
             self.run_import(data, 'auth.user',                  'User',         self.import_user)
             self.run_import(data, 'my_ortoloco.loco',           'Member',       self.import_member)  # must be after users
+
             self.run_import(data, 'my_ortoloco.depot',          'Depot',        self.import_depot)   # must be after members
             self.run_import(data, 'my_ortoloco.abo',            'Abo',          self.import_abo)     # must be after member and depot
             self.run_import(data, 'my_ortoloco.anteilschein',   'Anteilschein', self.import_anteilschein)     # must be after member
             self.run_import(data, 'my_ortoloco.taetigkeitsbereich',   'Kernbereich', self.import_taetigkeitsbereich)     # must be after member
+            self.run_import(data, 'my_ortoloco.jobtyp', 'Jobtyp', self.import_jobtyp)  # must be after members, aetigkeitsbereich
+            self.run_import(data, 'my_ortoloco.job', 'Jobs', self.import_job)  # must be after members, jobtyp
+            self.run_import(data, 'my_ortoloco.boehnli', 'Rüebli', self.import_assignment)  # must be after members, job
 
             self.run_import(data, 'my_ortoloco.loco',           'Member',       self.import_member_second_pass)  # must be after abo
             self.run_import(data, 'my_ortoloco.abo',            'Abo',          self.import_abo_second_pass)     # must be after loco_second_pass
 
             self.sql_sequence_reset()
+
+        print("=== Import done ===")
+
+    def create_job_extras(self):
+        extra = JobExtraType()
+        extra.name = "Auto"
+        extra.display_empty = "<img src='/static/img/auto_red.png' title='Auto benötigt' width='32' />"
+        extra.display_full = "<img src='/static/img/auto_green.png' title='Auto vorhanden' width='32' />"
+        extra.save()
 
     def create_abo_structure(self):
         product = SubscriptionProduct()
@@ -97,11 +106,24 @@ class Command(BaseCommand):
     def import_group(self, data):
         fields = data['fields']
 
+        # group.permissions are not imported but loaded from file
+        # To list the internal code name for permissions use:
+        # for p in Permission.objects.all(): print(p.codename + "\t\t" + p.name)
         group = Group(pk=data['pk'])
-
-        # intentionally not imported: group.permissions = []
         group.name = fields['name']
         group.save()
+
+        with open('initial_permissions.json') as json_file:
+            perm_data = json.load(json_file)
+
+        for key in perm_data:
+            if group.name  == key:
+                print(f"Importing {len(perm_data[key])} permissions for group '{key}'")
+                for perm_code in perm_data[key]:
+                    group.permissions.add(Permission.objects.get(codename=perm_code))
+
+        if group.name not in perm_data.keys():
+            print(f"No permissions found for group '{group.name }'")
 
     def get_datetime(self, string):
         return make_aware(datetime.datetime.fromisoformat(string))
@@ -118,15 +140,9 @@ class Command(BaseCommand):
         user.is_superuser = fields['is_superuser']
         user.is_staff = fields['is_staff']
         user.is_active = fields['is_active']
-        user.last_login  = self.get_datetime(fields['last_login'])
-        user.password  = fields['password']
-
+        user.last_login = self.get_datetime(fields['last_login'])
+        user.password = fields['password']
         user.save()
-
-        # intentionally not imported: user.user_permissions = []
-        if fields['groups']:
-            print("groups :")
-            print(fields['groups'])
 
         user.groups.set(fields['groups'])
 
@@ -155,37 +171,34 @@ class Command(BaseCommand):
     def import_anteilschein(self, data):
         fields = data['fields']
 
-        asch = Share(pk=data['pk'])
-
         if not fields['loco']:
-            print("No loco?")
+            print(f"Share pk='{data['pk']}' without member - skipped.")
             return
 
-        asch.member = Member.objects.get(pk=fields['loco'])
+        share = Share(pk=data['pk'])
+        share.member = Member.objects.get(pk=fields['loco'])
         if fields['paid']:
-            asch.paid_date = datetime.date.fromisoformat('2020-01-01')
-            asch.issue_date = datetime.date.fromisoformat('2020-01-01')
-            asch.booking_date = datetime.date.fromisoformat('2020-01-01')
+            share.paid_date = datetime.date.fromisoformat('2020-01-01')
+            share.issue_date = datetime.date.fromisoformat('2020-01-01')
+            share.booking_date = datetime.date.fromisoformat('2020-01-01')
         else:
-            asch.paid_date = None
-            asch.issue_date = None
-            asch.booking_date = None
+            share.paid_date = None
+            share.issue_date = None
+            share.booking_date = None
 
-        asch.cancelled_date = None
-        asch.termination_date = None
-        asch.payback_date = None
+        share.cancelled_date = None
+        share.termination_date = None
+        share.payback_date = None
         if fields['number']:
-            asch.number = fields['number']
+            share.number = fields['number']
             if int(fields['number']) != data['pk']:
-                print("number != pk ")
-                print(fields['number'])
-                print(data['pk'])
+                print(f"Share number {fields['number']} != pk {data['pk']}")
         else:
             # create a unique one, different from original numbers
-            asch.number = 1000000 + data['pk']
-        asch.sent_back = False
-        asch.notes = f'Imported from ID={data["pk"]}, number={fields["number"]}'
-        asch.save()
+            share.number = 1000000 + data['pk']
+        share.sent_back = False
+        share.notes = f'Imported from ID={data["pk"]}, number={fields["number"]}'
+        share.save()
 
     def import_taetigkeitsbereich(self, data):
         fields = data['fields']
@@ -265,6 +278,89 @@ class Command(BaseCommand):
         member.notes = 'Geschlecht: ' + fields['sex']
 
         member.save()
+
+    def import_jobtyp(self, data):
+        desc_verteilfahrt = """
+Du fährst zu den Depots. 
+Du informierst dich über die Route zu den Depots http://bioco.ch/intranet-dokumente und den
+aktuellen Verteilplan ("Verteilübersicht" im Intranet)
+
+Bitte fülle das Fahrspesen-Rückforderungsformular aus und sende es an Anna.
+
+Dieser Einsatz kann (muss aber nicht) mit dem vorhergehenden Verpacken gebucht werden.
+Dafür bitte bei beiden Einsätzen eintragen. 
+
+Der Zeitpunkt kann flexibel gewählt werden, so dass die Depots bis 16h beliefert sind, Start ab 11h.
+"""
+
+        desc_verpacken = """
+Wir ernten unser Gemüse und erledigen sonstige Arbeiten.
+Gute Schuhe und wetterfeste Kleidung anziehen.
+
+Dieser Einsatz kann (muss aber nicht) mit dem nachfolgenden Verteilen gebucht werden.
+Dafür bitte bei beiden Einsätzen eintragen. Es werden dann natürlich auch beide angerechnet.
+
+Die Anzahl Körbe entnehmt ihr dem aktuellen Verteilplan ("Verteilübersicht" im Intranet).
+
+Dauer ca. 2 - 3h."""
+
+        desc = {5: desc_verteilfahrt, 6: desc_verteilfahrt, 163: desc_verpacken}
+        if data['pk'] not in [5, 6, 163]: return
+
+        fields = data['fields']
+
+        JobType.objects.create(
+            pk = data['pk'],
+            name = fields['name'],
+            displayed_name = fields['displayed_name'],
+            description = desc[data['pk']],  # No HTML supported so far thus not using original field 'description'
+            default_duration = fields['duration'],
+            location = fields['location'],
+            activityarea = ActivityArea.objects.get(pk=fields['bereich'])
+        )
+
+        if fields['car_needed']:
+            JobExtra.objects.create(
+                recuring_type = JobType.objects.get(pk=data['pk']),
+                extra_type = JobExtraType.objects.get(pk=1)
+            )
+
+    def import_job(self, data):
+        fields = data['fields']
+
+        dt = self.get_datetime(fields["time"])
+        if dt < make_aware(datetime.datetime.fromisoformat('2021-01-01T00:00:00')):
+            return
+
+        RecuringJob.objects.create(
+            pk=data['pk'],
+            type = JobType.objects.get(pk=fields['typ']),
+            slots=fields["slots"],
+            time = dt,
+            multiplier = fields["multiplier"]
+        )
+
+    def import_assignment(self, data):
+        fields = data['fields']
+
+        try:
+            job = RecuringJob.objects.get(pk=fields['job'])
+        except:
+            # Could not find matching job - probably in the past
+            return
+
+        Assignment.objects.create(
+            pk=data['pk'],
+            job=job,
+            member=Member.objects.get(pk=fields['loco']),
+            amount=1
+        )
+
+        if fields['with_car']:
+            ass = Assignment.objects.get(pk=data['pk'])
+            car = JobExtra.objects.get(recuring_type=job.type)
+            #car = JobExtraType.objects.get(pk=1)
+            ass.job_extras.add(car)
 
     def import_member_second_pass(self, data):
         if data['pk'] == 1: return  # intranet admin
