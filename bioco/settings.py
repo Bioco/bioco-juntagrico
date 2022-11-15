f74b444151e9123d7a2eb3df98d2a766acbe1cfd
mbschenkel <github@markus-schenkel.ch>
2022-11-16 00:06:31 +0100

    Added a conditional logging config

--- bioco/settings.py
+++ bioco/settings.py
@@ -110,6 +110,42 @@ INSTALLED_APPS = (
 )
 
 
+
+"""
+    Logging Settings
+"""
+if os.environ.get("BIOCO_LOGGING"):
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                           'pathname=%(pathname)s lineno=%(lineno)s ' +
+                           'funcname=%(funcName)s %(message)s'),
+                'datefmt': '%Y-%m-%d %H:%M:%S'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            }
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            },
+        },
+        'loggers': {
+            'django.request': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        }
+    }
+
+
 """
     Email Settings
 """
