6758ad8165d8a83a1a6d57c4de70854504560c3c
mbschenkel <mbschenkel@gmail.com>
2025-01-22 22:49:12 +0100

    Reducing whitenoise setting (previous version did deploy)

--- bioco/settings.py
+++ bioco/settings.py
@@ -254,9 +254,7 @@ STATICFILES_FINDERS = (
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_USE_FINDERS = True
 WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_ALLOW_ALL_ORIGINS = True
 
 """
     Impersonate Settings
